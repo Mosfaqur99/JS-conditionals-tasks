185cf8d4167ac9201f3eb312b14e785d1ee1205a
Mosfaqur Rahman <mosfaqur99@gmail.com>
2025-02-09 16:59:09 +0600

    Gread Calculator

---
+++ Grade_Calculator.js
@@ -0,0 +1,18 @@
+var markes = 90;
+
+if(markes>=90 && markes <= 100){
+    console.log("You get A Gread")
+}
+else if(markes>=80 && markes <= 89){
+    console.log("You get B Gread")
+}
+else if(markes>=70 && markes <= 79){
+    console.log("You get C Gread")
+}
+else if(markes>=60 && markes <= 69){
+    console.log("You get D Gread")
+}
+else if(markes>0 && markes <= 59){
+    console.log("You get F Gread")
+}
+
